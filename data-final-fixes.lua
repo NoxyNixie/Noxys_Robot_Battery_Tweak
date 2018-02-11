6e6d0433661645c7302aed85b38792747038d5e7
Mark Sanders <mark.something@gmail.com>
2018-02-11 04:08:29 +0100

    Also support robots with 0kJ consumption (creative-mode).

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,6 +6,9 @@ local mul = settings.startup["Noxys_Robot_Battery_Tweak-multiplier"].value
 
 for _,r in pairs(robots) do
 	for _,v in pairs(data.raw[r]) do
-		v.max_energy = joules.nearest(joules.from(v.max_energy) * mul)
+		local j = joules.from(v.max_energy)
+		if j > 0 then
+			v.max_energy = joules.nearest(j * mul)
+		end
 	end
 end
